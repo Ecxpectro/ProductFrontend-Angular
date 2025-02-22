bb48772404665ff2e0ac5b6abac24dae243b742a
Ecxpectro <henriquescunha12345@gmail.com>
2025-02-21 23:06:13 -0300

    Integrate API with Signals for state management

--- src/app/products/components/product-list/product-list.component.ts
+++ src/app/products/components/product-list/product-list.component.ts
@@ -1,26 +1,52 @@
-import { Component, inject, OnInit } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button'
+import { Component, inject, OnInit, signal, ViewChild, viewChild, WritableSignal } from '@angular/core';
 import { ProductService } from '../../../services/product.service';
 import { Product } from '../../interfaces/products';
+import { CommonModule } from '@angular/common';
+
+//materials 
+import {MatButtonModule} from '@angular/material/button'
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator'
+import {MatTableDataSource, MatTableModule} from '@angular/material/table'
+
+
 @Component({
   selector: 'app-product-list',
-  imports: [MatButtonModule],
+  imports: [CommonModule, MatButtonModule, MatTableModule, MatPaginatorModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent implements OnInit {
   private productService = inject(ProductService);
 
-  products: Product[] = [];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  products: WritableSignal<Product[]> = signal<Product[]>([]);
+
+  displayedColumns: string[] = ["id", "description", "stock", "actions"];
+  dataSource = new MatTableDataSource<Product>([]);
 
   ngOnInit(): void {
-      this.productService.getProducts().subscribe({
-        next:(response) =>{
-          console.log(response);
-        },
-        error:(err) => {
-          console.log('Error getting products:', err)
-        }
-      });
+    this.loadProducts();
+  }
+
+  loadProducts(){
+    this.productService.getProducts().subscribe({
+      next:(products) => {
+        this.products.set(products);
+        this.updateTableData();
+      }
+    })
+  }
+
+  updateTableData(){
+    this.dataSource.data = this.products();
+    this.dataSource.paginator = this.paginator;
+  }
+
+  navigateToForm(id?:number){
+
+  }
+
+  deleteProduct(id?:number){
+
   }
 }
